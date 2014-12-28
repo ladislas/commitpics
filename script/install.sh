4e35bc8938f35c3785346ca83859eaeb72e13f06
ladislas <ladislas@weareleka.com>
2014-12-28 15:41:19 +0100

    add final output to explain what to do next

--- script/install.sh
+++ script/install.sh
@@ -7,7 +7,7 @@ read GITHUB_USERNAME
 # Set global variables
 GITHUB_DOMAIN="https://github.com"
 COMMIT_PICS_REPO_URL="$GITHUB_DOMAIN/$GITHUB_USERNAME/commitpics"
-GIT_COMMIT_PICS_PATH="$HOME/.git-commitpics-test"
+GIT_COMMIT_PICS_PATH="$HOME/.git-commitpics"
 GIT_COMMIT_PICS_SCRIPT_PATH="$GIT_COMMIT_PICS_PATH/script/post-commit.sh"
 GIT_TEMPLATES_PATH="$HOME/.git-templates"
 GIT_HOOKS_PATH="$GIT_TEMPLATES_PATH/hooks"
@@ -16,6 +16,7 @@ GIT_COMMIT_SCRIPT_PATH="$GIT_HOOKS_PATH/post-commit"
 # Make sure all the brew dependencies are met
 BREW_DEPS=( imagesnap imagemagick )
 
+echo "\nChecking for \`brew\` dependencies:\n "
 for dep in "${BREW_DEPS[@]}"
 do
 	if brew ls --version $dep &> /dev/null ; then
@@ -27,17 +28,21 @@ do
 done
 
 # Clone commitpics/commitpics to ~/.git-commit pics
-echo "Cloning the repository..."
-git clone $COMMIT_PICS_REPO_URL $GIT_COMMIT_PICS_PATH || exit 1;
+echo "\nCloning $COMMIT_PICS_REPO_URL to $GIT_COMMIT_PICS_PATH..."
+# git clone $COMMIT_PICS_REPO_URL $GIT_COMMIT_PICS_PATH || exit 1;
 
 # Create hooks
-echo "Creating ~/.git-templates/hooks..."
+echo "Creating $GIT_HOOKS_PATH..."
 if [[ ! -d  $GIT_HOOKS_PATH ]] ; then
 	mkdir -p $GIT_HOOKS_PATH
 fi
 
 # Symlink the post-commit script
-echo "Symlinking the post-commit script..."
-ln -s $GIT_COMMIT_PICS_SCRIPT_PATH $GIT_COMMIT_SCRIPT_PATH || exit 1
+echo "Symlinking $GIT_COMMIT_PICS_SCRIPT_PATH to $GIT_COMMIT_SCRIPT_PATH..."
+# ln -s $GIT_COMMIT_PICS_SCRIPT_PATH $GIT_COMMIT_SCRIPT_PATH || exit 1
 
-echo "End of the script"
+echo \
+"\nCongratulation! git-commitpics is now ready to be used!
+\`cd\` to a directory you want to track with commit pics and simply run:
+	\`git init\`
+You should be ready to go!"
